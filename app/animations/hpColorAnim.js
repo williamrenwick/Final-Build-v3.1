a4b27a03a106a5662e3789d9307919e508b9ffba
William Renwick <william.r.renwick@gmail.com>
2015-09-25 12:42:31 -0700

    homepage background-color

--- app/animations/hpColorAnim.js
+++ app/animations/hpColorAnim.js
@@ -36,7 +36,7 @@ function changeItemBg(scrollPos) {
 	var animationBeginPos = 0,
 		animationEndPos = $(document).height(),
 		beginningColor = new Color('rgb(197, 240, 247)'),
-		endingColor = new Color('rgb(229, 251, 255)');
+		endingColor = new Color('rgb(208, 234, 252)');
 
 
 	if (scrollPos >= animationBeginPos && scrollPos <= animationEndPos ) { 
