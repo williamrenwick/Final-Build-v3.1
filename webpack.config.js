d29c1dfbb24a2b75c11532b9ce32e4ea899378d8
William Renwick <william.r.renwick@gmail.com>
2015-10-05 00:41:20 +0100

    fixing hotloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,80 @@
-var webpack = require('webpack');
-var path = require('path');
+var Webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 var buildPath = path.resolve(__dirname, 'public', 'build');
 var mainPath = path.resolve(__dirname, 'app', 'main.js');
 
-module.exports = {
+var config = {
+
+  // Makes sure errors in console map to the correct file
+  // and line number
   devtool: 'eval',
   entry: [
-    'webpack/hot/dev-server', 
-    'webpack-dev-server/client?http://localhost:8080', 
-    mainPath
-  ],
+
+    // For hot style updates
+    'webpack/hot/dev-server',
+
+    // The script refreshing the browser on none hot updates
+    'webpack-dev-server/client?http://localhost:8080',
+
+    // Our application
+    mainPath],
   output: {
+
+    // We need to give Webpack a path. It does not actually need it,
+    // because files are kept in memory in webpack-dev-server, but an
+    // error will occur if nothing is specified. We use the buildPath
+    // as that points to where the files will eventually be bundled
+    // in production
     path: buildPath,
     filename: 'bundle.js',
+
+    // Everything related to Webpack should go through a build path,
+    // localhost:3000/build. That makes proxying easier to handle
     publicPath: '/build/'
   },
   module: {
+
     loaders: [
-      { 
-        test: /\.js$/, 
-        loader: 'babel',
-        exclude: [nodeModulesPath],
-      }, {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: path.resolve(__dirname, 'app')
-      }, {
-        test: /\.(png|jpg)$/,
-        loader: 'url?limit=25000'
-      }, {
+
+    // I highly recommend using the babel-loader as it gives you
+    // ES6/7 syntax and JSX transpiling out of the box
+    {
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: [nodeModulesPath]
+    },
+
+    // Let us also add the style-loader and css-loader, which you can
+    // expand with less-loader etc.
+    {
+      test: /\.css$/,
+      loader: 'style!css'
+    }, {
+      test: /\.(png|jpg)$/,
+      loader: 'url?limit=25000'
+    }, {
         test: /\.woff$/,
-        loader: 'url?limit=100000'
-      }
+        loader: 'url?limit=100000'      
+    }
+
+
     ]
   },
+
+  // We have to manually add the Hot Replacement plugin when running
+  // from Node
   plugins: [
-      new HtmlwebpackPlugin({ title: 'Wire Design' }),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.ProvidePlugin({
-        $ : "jquery",
-        jQuery : "jquery",
-        "window.jQuery" : "jquery",
-        "root.jQuery" : "jquery"
-      })
+    new Webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({ title: 'Wire Design' }),
+    new Webpack.ProvidePlugin({
+      $ : "jquery",
+      jQuery : "jquery",
+      "window.jQuery" : "jquery",
+      "root.jQuery" : "jquery"
+    })
   ]
 };
+
+module.exports = config;
