645a118fe2fe2e356f40045074fada57da98a2f5
William Renwick <william.r.renwick@gmail.com>
2015-10-12 20:31:08 +0100

    adding touch events

--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ var Router = require("react-router");
 var routes = require("./routing/routes");
 var stateTree = require('./data/stateTree.js');
 
+React.initializeTouchEvents(true)
 
 Router.run(routes, (Root, state) => {
   React.render(<Root {...state} tree={stateTree} />, document.body);
